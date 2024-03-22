47359f5594530f706e38235d508273932d806927
0248913 <142315920+0248913@users.noreply.github.com>
2024-03-22 16:56:01 +1100

    DA asset manager project

---
+++ assetManager02/AssetManagerApp/forms.py
@@ -0,0 +1,28 @@
+from turtle import textinput
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from .models import UserLog
+
+from django import forms
+
+from django.forms.widgets import PasswordInput, TextInput
+
+class CreateUserForm(UserCreationForm):
+    
+    class Meta:
+        
+        model = User
+        fields = ['username','email','password1','password2']
+        
+
+
+class LoginForm(AuthenticationForm):
+    
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+    
+class UserLogForm(forms.ModelForm):
+    
+    class Meta:
+        model = UserLog
+        fields = ['text']
---
+++ assetManager02/AssetManagerApp/views.py
@@ -0,0 +1,117 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from. forms import CreateUserForm, LoginForm
+
+from django.contrib.auth.models import auth
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
+from .forms import UserLogForm
+from .models import UserLog
+
+def homepage(request):
+    return render(request, "AssetManagerApp/index.html")
+
+
+def register(request):
+    
+    form = CreateUserForm
+    
+    if request.method == "POST":
+        
+        form = CreateUserForm(request.POST) 
+
+        if form.is_valid():
+         
+            form.save()
+         
+            return redirect("login")
+     
+    context = {'registerform':form}
+
+    return render(request, "AssetManagerApp/register.html", context=context)
+
+
+def login(request):
+    
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        
+        form = LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+         
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                
+                auth.login(request, user)
+                
+                return redirect("dashboard")
+            
+    context = {'loginform':form}
+    
+    return render(request, "AssetManagerApp/login.html",context=context)
+
+@login_required(login_url="login")
+def dashboard(request):
+    
+    if request.method == "POST":
+       form = UserLogForm(request.POST)
+       if form.is_valid():
+           log = form.save(commit=False)
+           log.user = request.user
+           log.save()
+           
+           return redirect('dashboard')   
+        
+    else:
+        form = UserLogForm()
+    
+    user_logs = UserLog.objects.filter(user=request.user)
+    return render(request,"AssetManagerApp/dashboard.html", {'form': form, 'user_logs': user_logs})
+
+
+def logout(request):
+    
+    auth.logout(request)
+    
+    return redirect("")
+    
+def newLog(request):
+    form = UserLogForm()
+    if request.method == "POST":
+        form = UserLogForm(request.POST)
+        if form.is_valid():
+            log = form.save(commit=False)
+            log.user = request.user
+            log.save()
+            return redirect('newLog')
+    user_logs = UserLog.objects.filter(user=request.user)
+    return render(request, "AssetManagerApp/newLog.html", {'form': form, 'user_logs': user_logs})
+   
+def editLog(request, log_id):
+    log = get_object_or_404(UserLog, id=log_id)
+
+    if request.method == 'POST':
+        form = UserLogForm(request.POST, instance=log)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    else:
+        form = UserLogForm(instance=log)
+    return render(request, "AssetManagerApp/editLog.html", {'form': form})    
+
+def deleteLog(request, log_id):
+    log = get_object_or_404(UserLog, id=log_id)
+    log.delete()
+    return redirect('dashboard')
+    
+  
+   
+ 
